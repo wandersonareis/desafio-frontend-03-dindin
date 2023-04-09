9f75defe1537f1ffcd3054c3248c95fd7f97ee56
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2023-04-09 02:34:46 -0300

    Modal editar perfil com componente UserForm

--- src/components/modal/UserPerfilEditModal.jsx
+++ src/components/modal/UserPerfilEditModal.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import InputStyled from "../basics/Input/InputStyled";
 import { LoadingButton } from "../buttons";
 import { schemaPasswordConfirmValidation } from "../../util/schemas";
 import { SpanWarning } from "../styled";
 import { getObjectItem, setObjectItem } from "../../util/storage";
 import { ModalBackdrop, ModalCloseButton, ModalContainer, ModalContentForm, ModalTitle } from "./modalStyled";
 import { handleErrors } from "../../handleErros";
-import { sendUserUpdateInfo } from "../../api";
+import { getUserInfo, sendUserUpdateInfo } from "../../api";
 import { useAuth } from "../../context";
 import { useFormInput } from "../../lib/customHooks";
+import { UserForm } from "../form";
 
 export default function UserPerfilEditModal({ onClose }) {
   const storedData = getObjectItem();
-  const { token, user, isLoading, setLoading } = useAuth();
+  const { token, user, setUserData, isLoading, setLoading } = useAuth();
   const { resetValue: resetName, ...name } = useFormInput(user.name);
   const { resetValue: resetEmail, ...email } = useFormInput(user.email);
   const { resetValue: resetPassword, ...password } = useFormInput("");
@@ -51,6 +51,9 @@ export default function UserPerfilEditModal({ onClose }) {
 
       setObjectItem({ ...storedData, nome: userData.name, email: userData.email });
 
+      const newUserData = await getUserInfo(token);
+      setUserData({ name: newUserData.nome, email: newUserData.email });
+
       resetAllFields();
       onClose();
     } catch (err) {
@@ -68,19 +71,12 @@ export default function UserPerfilEditModal({ onClose }) {
           <ModalTitle>Editar Perfil</ModalTitle>
           <ModalCloseButton onClick={onClose}>X</ModalCloseButton>
         </ModalContainer>
-        <InputStyled name="name" label="Nome" type="text" placeholder="Nome" onSelect={handleSelect} {...name} />
+        <UserForm handleSelect={handleSelect} name={name} email={email} password={password} confirmPassword={confirmPassword} />
         {warning.name && <SpanWarning>{warning.name}</SpanWarning>}
-
-        <InputStyled name="email" label="E-mail" type="text" placeholder="E-mail" onSelect={handleSelect} {...email} />
         {warning.email && <SpanWarning>{warning.email}</SpanWarning>}
-
-        <InputStyled name="password" label="Senha" type="password" placeholder="Senha" onSelect={handleSelect} {...password} />
         {warning.password && <SpanWarning>{warning.password}</SpanWarning>}
-
-        <InputStyled name="confirmPassword" label="Confirmação de senha" type="password" placeholder="Senha" onSelect={handleSelect} {...confirmPassword} />
         {warning.confirmPassword && <SpanWarning>{warning.confirmPassword}</SpanWarning>}
         <SpanWarning>{spanWarning}</SpanWarning>
-
         <LoadingButton type="submit" text="Confirmar" isLoading={isLoading} />
       </ModalContentForm>
     </ModalBackdrop>

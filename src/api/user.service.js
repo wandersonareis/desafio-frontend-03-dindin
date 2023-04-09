f7a74840eaeb59d4c5f0f7ba927fa1d0e945e85d
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2023-04-09 02:18:05 -0300

    user.service para async/await

--- src/api/user.service.js
+++ src/api/user.service.js
@@ -2,26 +2,30 @@ import api from "./api";
 import { removeItem } from "../util/storage";
 
 export async function getUserInfo(token) {
-  return await api.get("/auth/usuario", {
-    headers: { Authorization: `Bearer ${token}` }
+  const res = await api.get("/auth/usuario", {
+    headers: { Authorization: `Bearer ${token}` },
   });
+  return res.data;
 }
 
 export async function sendUserUpdateInfo(token, name, email, password) {
-  return await api.put("/auth/usuario",
+  return await api.put(
+    "/auth/usuario",
     { nome: name, email: email, senha: password },
     {
-      headers: { Authorization: `Bearer ${token}` }
+      headers: { Authorization: `Bearer ${token}` },
     }
   );
 }
 
-export function userLogin(email, password) {
-  return api.post("/login", { email, senha: password }).then(res => res.data);
+export async function userLogin(email, password) {
+  const res = await api.post("/login", { email, senha: password });
+  return res.data;
 }
 
-export function userSignUp(name, email, password) {
-  return api.post("/usuario", { nome: name, email, senha: password }).then(res => res.data);
+export async function userSignUp(name, email, password) {
+  const res = await api.post("/usuario", { nome: name, email, senha: password });
+  return res.data;
 }
 
 export function userLogout() {

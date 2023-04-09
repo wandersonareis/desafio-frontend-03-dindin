f3e975c7407e507a61ba883926aeb4a427dd814a
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2023-04-09 02:33:30 -0300

    Criado componente userForm para o form de criar e editar usuário

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,5 +1,6 @@
 import SignUpForm from "./signUpForm";
 import SignInForm from "./signInForm";
+import UserForm from "./userForm";
 import TransactionForm from "./transactionForm";
 
-export { SignUpForm, SignInForm, TransactionForm };
+export { SignUpForm, SignInForm, UserForm, TransactionForm };
---
+++ src/components/form/userForm.jsx
@@ -0,0 +1,54 @@
+import { createInputs } from "../basics/Input/createInputs";
+
+export default function UserForm({ handleSelect, name, email, password, confirmPassword }) {
+
+  const inputModels = [
+    {
+      inputType: "input",
+      name: "name",
+      label: "Nome",
+      placeholder: "Digite seu nome",
+      type: "text",
+      onSelect: handleSelect,
+      ...name,
+    },
+    {
+      inputType: "input",
+      name: "email",
+      label: "E-mail",
+      placeholder: "Digite seu e-mail",
+      type: "email",
+      pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
+      title: "Insira um endereço de email válido.",
+      onSelect: handleSelect,
+      autocomplete: "email",
+      ...email,
+    },
+    {
+      inputType: "input",
+      name: "password",
+      label: "Senha",
+      placeholder: "Digite sua senha",
+      type: "password",
+      onSelect: handleSelect,
+      required: true,
+      autocomplete: "new-password",
+      ...password,
+    },
+    {
+      inputType: "input",
+      name: "confirmPassword",
+      label: "Senha",
+      placeholder: "Confirme sua senha",
+      type: "password",
+      onSelect: handleSelect,
+      required: true,
+      autocomplete: "new-password",
+      ...confirmPassword,
+    },
+  ];
+
+  const inputs = createInputs(inputModels);
+
+  return inputs;
+}
